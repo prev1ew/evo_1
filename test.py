2bb6d4fff600541c4eacee4e50df4e377c9bb4fd
prev1ew <49633811+prev1ew@users.noreply.github.com>
2022-01-15 22:18:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ def direction(facing, turn):
     step = 45
 
     facings = {
-        0:'N',
-        1:'NE',
-        2:'E',
-        3:'SE',
-        4:'S',
-        5:'SW',
-        6:'W',
-        7:'NW',
+        0: 'N',
+        1: 'NE',
+        2: 'E',
+        3: 'SE',
+        4: 'S',
+        5: 'SW',
+        6: 'W',
+        7: 'NW',
     }
 
     facing_upper = facing.upper()
